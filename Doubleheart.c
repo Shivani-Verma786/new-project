04257cba26cf99311662bbef853a24838fa6b9fb
Shivani Verma <64006038+Shivani-Verma786@users.noreply.github.com>
2020-04-28 14:17:30 +0530

    Add files via upload

---
+++ Doubleheart.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    printf("Enter number = ");
+    scanf("%d",&n);
+    printf("\n\n");
+    for(i=n/2 ; i<=n ; i+=2)
+    {
+        for(j=1 ; j<=n-i ; j+=2)
+        { printf(" "); }
+        for(j=1 ; j<=i ; j++)
+        { printf("*"); }
+        for(j=1 ; j<=n-i ; j+=2)
+        { printf("  "); }
+        for(j=1 ; j<=i ; j++)
+        { printf("*"); }
+        for(j=1 ; j<=n-i ; j+=2)
+        { printf("  "); }
+        for(j=1 ; j<=i ; j++)
+        { printf("*"); }
+        for(j=1 ; j<=n-i ; j+=2)
+        { printf("  "); }
+        for(j=1 ; j<=i ; j++)
+        { printf("*"); }
+        printf("\n");
+    }
+    for(i=n ; i>=1 ; i--)
+    {
+        for(j=1 ; j<=n-i ;j++)
+        { printf(" ");}
+        for(j=1 ; j<=(i*2)-1 ; j++)
+        { printf("*");}
+        for(j=1 ; j<= n-i+1; j++)
+        { printf("  ");}
+        for(j=1 ; j<=(i*2)-1 ; j++)
+        { printf("*");}
+        printf("\n");
+    }
+
+    return 0;
+}
